dc90add6047dd46782681730d718651268625b72
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-27 11:57:42 +0530

    BTRYHLTH

---
+++ Battery-Health.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+      	Scanner sc = new Scanner(System.in);
+      	int T = sc.nextInt();
+      	int i;
+        
+      	for(i=1; i<=T; i++){
+      	    int X = sc.nextInt();
+          
+      	    if(X >= 80){
+      	        System.out.println("YES");
+      	    }
+      	    else{
+      	        System.out.println("NO");
+      	    }
+      	}
+	}
+}
