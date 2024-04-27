f4a9506f94af16a50958dc167de4660f9978d451
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-27 12:00:40 +0530

    AGEING

---
+++ Ageing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	int i,s;
+        
+    	for(i=1; i<=T; i++){
+    	    int X = sc.nextInt();
+    	    s = X - 10;
+    	    System.out.println(s);
+    	}
+	}
+}
