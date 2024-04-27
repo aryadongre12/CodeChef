fd72ebd3548a5edfe27f45bc3f3a4d7b1c481604
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-27 12:05:25 +0530

    READPAGES

---
+++ Read-Pages.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	int i, p;
+    	for(i=1; i<=T; i++){
+    	    int N = sc.nextInt();
+    	    int X = sc.nextInt();
+    	    int Y = sc.nextInt();
+    	    p = X * Y;
+            
+    	    if(p >= N){
+    	        System.out.println("YES");
+    	    }
+    	    else{
+    	        System.out.println("NO");
+    	    }
+	    }
+	}
+}
