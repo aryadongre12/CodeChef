0fea32d6817d4cdefeeb807d084174ee3e389751
Arya Dongre <165511129+aryadongre12@users.noreply.github.com>
2024-04-27 12:07:00 +0530

    REACH_HOME

---
+++ Reach-Home.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main (String[] args) throws java.lang.Exception{
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	int i, s;
+        
+    	for(i=1; i<=T; i++){
+    	    int X = sc.nextInt();
+    	    int Y = sc.nextInt();
+    	    s = X * 5;
+            
+    	    if(s >= Y){
+    	        System.out.println("YES");
+    	    }
+    	    else{
+    	        System.out.println("NO");
+    	    }
+    	}
+	}
+}
